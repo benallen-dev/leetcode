6bbf5170d13db001d3badb7cb45aa5f937a4cb57
Ben Allen <ben@benallen.dev>
2023-11-16 08:59:14 +0100

    Approximately halve the runtime of #22

--- 22-parentheses/main.ts
+++ 22-parentheses/main.ts
@@ -1,22 +1,35 @@
 // https://leetcode.com/problems/generate-parentheses/?envType=study-plan-v2&envId=top-100-liked
 
+type ResultNode = {
+	open: number;
+	closed: number;
+	value: string;
+}
+
 function generateParenthesis(n: number): string[] {
-	let results: string[] = [""];
+	let results: ResultNode[] = [{
+		open: 0,
+		closed: 0,
+		value: ""
+	}];
 
-	for (let i = 0; i < 2*n; ++i) {
+	for (let i = 0; i < 2 * n; ++i) {
 		const newResults = results.flatMap(result => {
-			let open = 0;
-			let closed = 0;
 
-			for (const letter of result) {
-				if (letter === "(") open++;
-				if (letter === ")") closed++;
-			}
+			console.log({ result });
 
-			let foo:string[] = [];
+			let foo: ResultNode[] = [];
 
-			if (open < n) foo.push(result + "(");
-			if (open > closed) foo.push(result + ")");
+			if (result.open < n) foo.push({
+				open: result.open + 1,
+				closed: result.closed,
+				value: result.value + "("
+			});
+			if (result.closed < result.open) foo.push({
+				open: result.open,
+				closed: result.closed + 1,
+				value: result.value + ")"
+			});
 
 			return foo;
 		});
@@ -24,7 +37,7 @@ function generateParenthesis(n: number): string[] {
 		results = newResults;
 	}
 
-	return results;
+	return results.map(x => x.value);
 };
 
 console.log(generateParenthesis(2));
